d3e3218f495e7e6bcaa14070c457861fa9047ffc
bokamatyas <boka.matyas@students.jedlik.eu>
2023-06-02 10:50:18 +0200

    ui.js, projectile.js + base functions, shooting

--- classes/player.js
+++ classes/player.js
@@ -1,25 +1,56 @@
-export default class Player{
+import Projectile from "./projectile.js";
 
-    constructor(x, y, radius) {
-        this.x = x;
-        this.y = y;
-        this.radius = radius;
+export default class Player {
+  constructor(x, y, radius) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+  }
 
+  // Játékos megrajzolása (ToDo: játékos grafika)
+  draw(context) {
+    context.beginPath();
+    context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    context.fillStyle = "rgba(0, 0, 0, 1)";
+    context.fill();
+  }
+
+  shoot(event, mouseClientY, mouseClientX, player, projectiles) {
+    if (event.code == "KeyF") {
+      const angle = Math.atan2(
+        mouseClientX - player.x,
+        mouseClientY - player.y        
+      );
+
+      const speedModifier = 6;
+
+      const velocity = {
+        x: Math.cos(angle) * speedModifier,
+        y: Math.sin(angle) * speedModifier,
+      };
+
+      projectiles.push(
+        new Projectile(player.x, player.y, 5, "rgba(255, 215, 0, 1)", velocity)
+      );
     }
+    return projectiles;
+  }
 
-    // Játékos megrajzolása (ToDo: játékos grafika)
-    draw(context) {
-
-        context.beginPath();
-        context.arc(
-            this.x,
-            this.y,
-            this.radius,
-            0, Math.PI * 2,
-            false
-        );
-        context.fillStyle = 'rgba(0, 0, 0, 1)';
-        context.fill();
-
-    }
+  evaluateProjectiles(context, projectiles, canvas){
+    projectiles.forEach((projectile, projectileIndex) => {
+        projectile.update(context);
+  
+        if (
+          projectile.x + projectile.radius < 0 ||
+          projectile.x - projectile.radius > canvas.width ||
+          projectile.y + projectile.radius < 0 ||
+          projectile.y - projectile.radius > canvas.height
+        ) {
+          setTimeout(() => {
+            projectiles.splice(projectileIndex, 1);
+          }, 0);
+        }
+      });
+    return projectiles;
+  }
 }
--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 import Player from "./classes/player.js";
+import UI from "./classes/ui.js";
 
 window.addEventListener('load', function (){
 
@@ -11,6 +12,34 @@ window.addEventListener('load', function (){
 
     const player = new Player(canvas.width * 0.5, canvas.height - canvas.height * 0.1, 20);
 
-    // ideiglenes
-    player.draw(context);
+    const ui = new UI;
+
+    let projectiles = [];
+
+    // Framek    
+    let animationId;
+    function animate(){
+
+        animationId = requestAnimationFrame(animate);
+
+        player.draw(context);
+        player.evaluateProjectiles(context, projectiles, canvas);
+        ui.clearCanvas(context, canvas);
+        console.log(projectiles);
+    }
+
+    // egér koordináták
+    let mouseClientX;
+    let mouseClientY;
+    this.window.addEventListener("pointermove", (e) => {
+        mouseClientX = e.clientX;
+        mouseClientY = e.clientY;
+    });
+
+    // tüzelés listener
+    window.addEventListener("keyup", (event) => {
+        projectiles = player.shoot(event, mouseClientX, mouseClientY, player, projectiles);
+    });
+
+    animate();
 });
